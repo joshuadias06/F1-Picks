8e3fba6e978ee1cccd076dbcabf516720ca1961a
joshuadias06 <joshuadiasbf@gmail.com>
2025-09-17 18:06:55 -0300

    This commit introduces a custom F1 font family and applies it to the application's typography and a custom button component.

--- app/src/main/java/com/terminal/f1picks/presentation/components/CustomButton.kt
+++ app/src/main/java/com/terminal/f1picks/presentation/components/CustomButton.kt
@@ -9,10 +9,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.terminal.f1picks.ui.theme.F1FontFamily
+import com.terminal.f1picks.ui.theme.F1PicksTheme
 import com.terminal.f1picks.ui.theme.White
 
 @Composable
@@ -21,21 +24,17 @@ fun CustomButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val gradient = Brush.linearGradient(
+    val gradient = Brush.horizontalGradient(
         colorStops = arrayOf(
-            0.0f to Color(0xFFFF003C),
-            0.7f to Color(0xFFFF003C),
+            0.0f to Color(0xFF990022),
+            0.5f to Color(0xFFFF003C),
             1.0f to Color(0xFF990022)
-        ),
-        start = Offset(0f, 0f),
-        end = Offset(1000f, 0f)
+        )
     )
 
     Button(
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Transparent
-        ),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
         shape = RoundedCornerShape(16.dp),
         modifier = modifier
             .padding(horizontal = 32.dp)
@@ -43,8 +42,21 @@ fun CustomButton(
             .background(brush = gradient, shape = RoundedCornerShape(16.dp))
     ) {
         Text(
+            fontFamily = F1FontFamily,
+            fontWeight = FontWeight.Normal,
             text = text,
             color = White
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomButtonPreview() {
+    F1PicksTheme {
+        CustomButton(
+            text = "Começar",
+            onClick = {}
         )
     }
 }
--- app/src/main/java/com/terminal/f1picks/ui/theme/Type.kt
+++ app/src/main/java/com/terminal/f1picks/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package com.terminal.f1picks.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.terminal.f1picks.R
 
-// Set of Material typography styles to start with
+
+val F1FontFamily = FontFamily(
+    Font(R.font.f1_regular, FontWeight.Normal),
+    Font(R.font.f1_bold, FontWeight.Bold),
+    Font(R.font.f1_wide, FontWeight.Medium),
+    Font(R.font.f1_black, FontWeight.Black),
+    Font(R.font.f1_italic, FontWeight.Thin),
+    Font(R.font.f1_bold_web, FontWeight.ExtraBold)
+)
+
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = F1FontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = F1FontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = F1FontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
